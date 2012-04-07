f2918c75795e53818b7569b010f33944fbe741e1
David Newton <david.newton@asu.edu>
2012-04-07 14:24:15 -0700

    updated course.h .cpp files

--- CSE360Project/Course.cpp
+++ CSE360Project/Course.cpp
@@ -1,10 +1,59 @@
+// Created by David Newton
+
+// Quiz.h is the header file for the Quiz.cpp file, which will is used to hold information on a specific quiz
+
+// File created on 5 April 2012.
+
 #include "stdafx.h"
 #include "Course.h"
 
 
 namespace CSE360Project {
-Course::Course(){
-	test =1;
-}
+
+	
+		Course::Course(){
+			
+		}
+
+		void Course::setName(string courseName){
+
+		}
+
+		string Course::getName(){
+			return "test";
+		}
+
+		void Course::setStudents(int studentIDList[]){
+
+		}
+
+		string Course::getStudents(){
+			return "test";
+		}
+
+		int* Course::getQuizList(){
+			int* pointer;
+			return pointer;
+		}
+
+		// Quiz Course::getQuiz();
+
+		double* Course::getGrades(){
+			return NULL;
+		}
+
+		//Quiz Course::creatQuiz();
+
+		void Course::deleteQuiz(){
+
+		}
+
+		void Course::deleteStudents(){
+
+		}
+
+		int Course::addCourseData(int userID, string courseName){
+			return 1;
+		}
 
 }
--- CSE360Project/Course.h
+++ CSE360Project/Course.h
@@ -1,16 +1,35 @@
 
+// Created by David Newton
 
+// Quiz.h is the header file for the Quiz.cpp file, which will is used to hold information on a specific quiz
+
+// File created on 5 April 2012.
 
 #pragma once
 #include "stdafx.h"
 
-
+using namespace std;
 
 namespace CSE360Project {
 	class Course {
 	public:
-		Course ();
-		int test;
+		Course();
+		void setName(string courseName);
+		string getName();
+		void setStudents(int studentIDList[]);
+		string getStudents();
+		int* getQuizList();
+		// Quiz getQuiz();
+		double* getGrades();
+		//Quiz creatQuiz();
+		void deleteQuiz();
+		void deleteStudents();
+		int addCourseData(int userID, string courseName);
+
+	protected:
+		int courseID;
+		string courseName;
+		int quizList[];
 
 	};
 }
